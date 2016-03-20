f9ccb07270a0703f2caa8cb0ca5b7d32c35902c5
liebesu <lichunjiang42@gmail.com>
2016-03-20 20:39:52 +0800

    update
    update

--- find_fang.py
+++ find_fang.py
@@ -2,7 +2,7 @@
 import requests
 from bs4 import BeautifulSoup
 __author__ = 'admin'
-urllist=['http://esf.nanjing.fang.com/house/g23-j2134-k2138-h31-l3010-kw%cb%ae%d4%c2%c7%d8%bb%b4/']
+urllist=['http://esf.nanjing.fang.com/house/g23-j2134-k2138-h31-j3100-l3010-kw%cb%ae%d4%c2%c7%d8%bb%b4/']
 
 
 
@@ -15,8 +15,13 @@ def get_info():
     a.write(m)
     a.close()
     soup=BeautifulSoup(m,"html.parser")
-    for price in soup.find_all(class_="price"):
+    contens=soup.find_all(class_="list rel")
+    print len(contens)
+    print contens
+    '''for price in contens:
         print price.get_text()
+        pass
+        #print price.get_text()'''
 def check_db():
     pass
 
